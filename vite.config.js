fb0452ad7063e78a4d3fa836e23e20f8310173ef
ell20-sys <elliotawe@outlook.com>
2025-04-03 14:56:14 +0000

    added base there

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/joke-gen/',
+  base: '/joke-gen/',
   plugins: [
     tailwindcss(),
     react()
